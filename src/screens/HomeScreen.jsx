53a79da4da7dfd21de89438b206038e1220e9ab9
haohaodu <hdu2899@gmail.com>
2020-10-20 00:37:21 -0400

    finished setting up intial home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -3,14 +3,22 @@ import "../css/screens/HomeScreen.css"
 import homeBackground from "../images/background/homeBackground.jpg";
 import volunteer from "../images/background/volunteer.jpg";
 import trackstar from "../images/background/trackstar.jpg";
+import email from "../images/logo/email.png";
+import github from "../images/logo/github.png";
+import linkedin from "../images/logo/linkedin.png";
 import { Container, Row, Col } from 'react-bootstrap';
 import SingleProject from "../components/SingleProject";
-import acac from "../images/background/acac.jpg"
-import Typewriter from "typewriter-effect"
+import acac from "../images/background/acac.jpg";
+import Typewriter from "typewriter-effect";
+import curbc from "../images/background/curbc.jpg";
 import OpeningAnimation from "../components/OpeningAnimation.jsx";
+
 // import haohao from "../images/people/haohao.jpg"
 
 const HomeScreen = () => {
+
+    const logos = [email, github, linkedin]
+
     return (
         <div className="page">
             <div className="mastHead-container">
@@ -18,10 +26,11 @@ const HomeScreen = () => {
                 <div className="mastHead">
                     <div className="mast-details">
                         {/* <img src={haohao} style={{ width: "20%", height: "auto", borderRadius: "25" }} alt="" /> */}
+
                         <h1>Hi, I'm HaoHao.</h1>
-                        <strong>
+                        <strong style={{ textAlign: "center" }}>
                             {/* <OpeningAnimation /> */}
-                            {/* <Typewriter
+                            <Typewriter
                                 onInit={(typewriter) => {
                                     typewriter.changeDelay(0.2)
                                 }}
@@ -30,16 +39,22 @@ const HomeScreen = () => {
                                     autoStart: true,
                                     loop: true,
                                 }}
-                            /> */}
-                            <p>Welcome to my portfolio.</p>
+                            />
+                            {/* <p>Welcome to my portfolio.</p> */}
                         </strong>
+                        <div style={{ display: "flex", flexDirection: "row", marginTop: 15 }}>
+                            {logos.map((logo, i) => {
+                                let marginRight = i === logos.length - 1 ? 0 : 25;
+                                return <img style={{ marginRight: marginRight, height: 25, width: 25 }} src={logo} />
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="projects">
                 <Container fluid>
                     <Row style={{ height: "300px" }}>
-                        <Col className="mt-1 mr-1">
+                        <Col className="mt-1 mr-0">
                             <SingleProject title="iContribute" photo={volunteer} roles="Founder and Tech Lead" />
                         </Col>
                         <Col className="mt-1 ml-1">
@@ -47,8 +62,11 @@ const HomeScreen = () => {
                         </Col>
                     </Row>
                     <Row style={{ height: "300px" }}>
-                        <Col className="mt-1 ml-1">
+                        <Col className="mt-1 mr-0">
                             <SingleProject title="ACAC" photo={acac} roles="Web Designer and Developer" />
+                        </Col>
+                        <Col className="mt-1 ml-1">
+                            <SingleProject title="CuHacking 2020" photo={curbc} roles="Backend Developer" />
                         </Col>
                     </Row>
                 </Container>
