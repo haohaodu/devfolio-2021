511bcb9576502fd25d131d954e007481dcd31f8e
haohaodu <hdu2899@gmail.com>
2020-10-24 23:04:10 -0400

    got the date to work on iContribute

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../css/components/SingleProject.css"
 
 const SimpleProject = (props) => {
     return (
         <div className="single-project" >
-            
+
             <a className="project-cover" href={props.route}>
-                <img style={{width: "100%", height: "100%", objectFit: "cover"}} alt="project background" src={props.photo}  />
+                <img className="project-photo" alt="project background" src={props.photo} />
             </a>
             <div className={"project-details"}>
                 <div className="project-title">
--- src/screens/projects/iContribute.jsx
+++ src/screens/projects/iContribute.jsx
@@ -3,17 +3,41 @@ import "../../css/screens/projects/icontribute.css"
 import collage from "../../images/images/icontribute/collage.png";
 import volunteer from "../../images/background/volunteer.jpg";
 
-const iContribute = () => { 
+const iContribute = () => {
+
+    const monthDiff = (d1, d2) => {
+        var months;
+        months = (d1.getFullYear() - d2.getFullYear()) * 12;
+        months -= d2.getMonth();
+        months += d1.getMonth();
+        return months <= 0 ? 0 : months;
+    }
+
+
+    const getCurrentMonth = () => {
+        return (monthDiff(new Date(), new Date("April 1, 2020 11:10:00"))).toString()
+
+    }
+
     return (
         <div className="icontribute-page">
             <h1>iContribute</h1>
-            <p>Product Founder and Tech Lead</p>
-            <div style={{width: "100%", height: "20%"}}>
-                <img style={{ objectFit: "cover", width: "100%", height: "100%" }} src={volunteer} />
+            <div className="header-two">Product Founder and Tech Lead</div>
+            <div className="image-container">
+                <img className="image" src={volunteer} />
             </div>
-            <p>This is how we came to be</p>
+            <div className="intro-container">
+                <div className="intro-date">Duration: {getCurrentMonth()} Months (ongoing)</div>
+                <div className="intro-text">
+                    <div>Research Phase: HaoHao Du, Umai Balendra, James Ying</div>
+                    <div>Design Phase: Carmen Lin</div>
+                    <div>Development Phase: HaoHao Du, James Ying, Kirti Desai, Umang Srivastav, Alex Gold, Lydia Pitts </div>
+                    <div>Maintenance Phase: HaoHao Du</div>
+                </div>
+
+            </div>
             <p>This is how we made sure to have fun too!</p>
-            <div style={{position:"relative", width: "100%", height: "auto"}}>
+            <div style={{ position: "relative", width: "100%", height: "auto" }}>
                 <img style={{ objectFit: "cover", width: "100%", height: "100%" }} src={collage} />
             </div>
         </div>
